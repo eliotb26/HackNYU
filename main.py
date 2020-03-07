f117d325abc843503ed57e7d5b7fce33e1c5c435
Rtlyc <49352204+Rtlyc@users.noreply.github.com>
2020-03-07 02:22:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -66,6 +66,46 @@ def detect_document(path):
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
+        
+def detect_document_with_newLine():
+    import os,io
+    from google.cloud import vision
+    from google.cloud.vision import types
+    import pandas as pd
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r'Hand write code-1f0460a79a72.json'
+    client = vision.ImageAnnotatorClient()
+
+    file_name = 'IMG_0028.jpg'
+    image_path = f'/Users/yuchen/PycharmProject/nyuHack/{file_name}'
+
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    # construct an iamge instance
+    image = vision.types.Image(content=content)
+
+    """
+    # or we can pass the image url
+    image = vision.types.Image()
+    image.source.image_uri = 'https://edu.pngfacts.com/uploads/1/1/3/2/11320972/grade-10-english_orig.png'
+    """
+
+    # annotate Image Response
+    response = client.document_text_detection(image=image)  # returns TextAnnotation
+    df = pd.DataFrame(columns=['locale', 'description'])
+
+    texts = response.text_annotations
+    for text in texts:
+        df = df.append(
+            dict(
+                locale=text.locale,
+                description=text.description
+            ),
+            ignore_index=True
+        )
+
+    print(df['description'][0])
 
 
 def main():
